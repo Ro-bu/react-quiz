f652d0753222fd36d22aad93453c2f53ba52b69b
Ro-bu <robin.raid1@gmail.com>
2022-06-07 12:37:34 +0300

    added fisher-yates shuffle to randomize the order of answers

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default function Question (props) {
+    // Fisher-Yates shuffle
+    function shuffle(array) {
+        let m = array.length, t, i;
+        while (m) {
+          i = Math.floor(Math.random() * m--);
+          t = array[m];
+          array[m] = array[i];
+          array[i] = t;
+        }
+        return array;
+    }
     let answers = [...props.falseAnswers, props.rightAnswer];
+    shuffle(answers);
     let answerElements = answers.map((answer) => {
         return (
             <div key={answer} className="answer">
